d936d9a07e057340ab0523fcc5838d61f57b3ecb
陈凯 <742931485@qq.com>
2016-10-10 14:05:03 +0800

    新建loginstroyboard

---
+++ ArrangeMovie/viewcontrollers/login/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  ArrangeMovie
+//
+//  Created by 陈凯 on 16/10/10.
+//  Copyright © 2016年 EMI. All rights reserved.
+//
+
+#import "EMIBaseViewController.h"
+
+@interface LoginViewController : EMIBaseViewController
+
+@end
