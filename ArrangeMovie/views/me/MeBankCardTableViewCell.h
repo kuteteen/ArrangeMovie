dc21828dbc2ad046137e5176a2584efba4e89794
WongSuechang <semp@live.cn>
2016-10-10 18:36:49 +0800

    "我的"--new Cell 2016-10-10 18:36:43

---
+++ ArrangeMovie/views/me/MeBankCardTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MeBankCardTableViewCell.h
+//  ArrangeMovie
+//
+//  Created by Emi-iMac on 16/10/10.
+//  Copyright © 2016年 EMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeBankCardTableViewCell : UITableViewCell
+
+@end
---
+++ ArrangeMovie/views/me/MeSettingTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MeSettingTableViewCell.h
+//  ArrangeMovie
+//
+//  Created by Emi-iMac on 16/10/10.
+//  Copyright © 2016年 EMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeSettingTableViewCell : UITableViewCell
+
+@end
