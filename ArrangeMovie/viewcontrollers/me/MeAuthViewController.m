9337eedcc8bb99647bcbc5b2f100294dff41a911
WongSuechang <semp@live.cn>
2016-10-17 09:58:05 +0800

    no message

--- ArrangeMovie/viewcontrollers/me/MeAuthViewController.m
+++ ArrangeMovie/viewcontrollers/me/MeAuthViewController.m
@@ -11,8 +11,8 @@
 #import "SCSlidePageView.h"
 //#import "EMIShadowImageView.h"
 
-#define width [UIScreen mainScreen].bounds.size.width
-#define height [UIScreen mainScreen].bounds.size.height
+#define Width [UIScreen mainScreen].bounds.size.width
+#define Height [UIScreen mainScreen].bounds.size.height
 
 @interface MeAuthViewController ()<SCFadeSlideViewDelegate,SCFadeSlideViewDataSource>
 
@@ -30,35 +30,38 @@
     }else{
         self.title = @"认证院线经理";
     }
+    
+    self.array = [[NSMutableArray alloc] init];
     [self initViewsWithUserType:self.user.usertype];
+    
 }
 
 -(void)initViewsWithUserType:(int)type {
     //添加滑动的图片浏览
-//    SCFadeSlideView *slideView = [[SCFadeSlideView alloc] initWithFrame:CGRectMake(0, 0, width, height-143)];
-//    slideView.backgroundColor = [UIColor clearColor];
-//    slideView.delegate = self;
-//    slideView.datasource = self;
-//    slideView.minimumPageAlpha = 0.4;
-//    slideView.minimumPageScale = 0.85;
-//    
-//    ///添加初始的添加按钮图片
-//    slideView.orginPageCount = 1;
-//    slideView.orientation = SCFadeSlideViewOrientationHorizontal;
-//    
-//    /****************************
-//     使用导航控制器(UINavigationController)
-//     如果控制器中不存在UIScrollView或者继承自UIScrollView的UI控件
-//     请使用UIScrollView作为SCFadeSlideView的容器View,才会显示正常,如下
-//     *****************************/
-//    UIScrollView *bottomScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, width, height-143)];
-//    [bottomScrollView addSubview:slideView];
-//    [self.view addSubview:bottomScrollView];
+    SCFadeSlideView *slideView = [[SCFadeSlideView alloc] initWithFrame:CGRectMake(0, 0, Width, Height-143)];
+    slideView.backgroundColor = [UIColor clearColor];
+    slideView.delegate = self;
+    slideView.datasource = self;
+    slideView.minimumPageAlpha = 0.4;
+    slideView.minimumPageScale = 0.85;
+    
+    ///添加初始的添加按钮图片
+    slideView.orginPageCount = 1;
+    slideView.orientation = SCFadeSlideViewOrientationHorizontal;
+    
+    /****************************
+     使用导航控制器(UINavigationController)
+     如果控制器中不存在UIScrollView或者继承自UIScrollView的UI控件
+     请使用UIScrollView作为SCFadeSlideView的容器View,才会显示正常,如下
+     *****************************/
+    UIScrollView *bottomScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, Width, Height-143)];
+    [bottomScrollView addSubview:slideView];
+    [self.view addSubview:bottomScrollView];
     
     
     if(type==0){
         //片方,添加圆形打钩按钮
-        EMIShadowImageView *OKImgView = [[EMIShadowImageView alloc] initWithFrame:CGRectMake((width-58)/2, height-110, 58, 58)];
+        EMIShadowImageView *OKImgView = [[EMIShadowImageView alloc] initWithFrame:CGRectMake((Width-58)/2, Height-110, 58, 58)];
         [OKImgView setShadowWithType:EMIShadowPathRound shadowColor:[UIColor blackColor] shadowOffset:CGSizeZero shadowOpacity:0.3 shadowRadius:10 image:@"" placeholder:@"row_piece_review"];
 //        OKImgView setHighlightedImage:[UIImage imageNamed:row]
         [self.view addSubview:OKImgView];
@@ -92,7 +95,7 @@
 
 #pragma mark SCFadeSlideView delegate
 -(CGSize)sizeForPageInSlideView:(SCFadeSlideView *)slideView {
-    return CGSizeMake(width, height-143);
+    return CGSizeMake(Width, Height-143);
 }
 
 #pragma mark SCFadeSlideView datasource
@@ -103,10 +106,28 @@
 -(UIView *)slideView:(SCFadeSlideView *)slideView cellForPageAtIndex:(NSInteger)index {
     SCSlidePageView *pageView = (SCSlidePageView *)[slideView dequeueReusableCell];
     if(!pageView){
-        pageView = [[SCSlidePageView alloc] initWithFrame:CGRectMake(0, 0, width - 84, height-110)];
+        pageView = [[SCSlidePageView alloc] initWithFrame:CGRectMake(0, 0, Width - 84, Height-110)];
         pageView.layer.cornerRadius = 4;
         pageView.layer.masksToBounds = YES;
+        pageView.backgroundColor = [UIColor clearColor];
+        pageView.coverView.backgroundColor = [UIColor clearColor];
         
+        EMIShadowImageView *shadowImageView;
+        shadowImageView = [[EMIShadowImageView alloc] initWithFrame:pageView.frame];
+        shadowImageView.contentMode = UIViewContentModeScaleAspectFit;
+        
+        if(self.array.count>0&&index<self.array.count-1){
+            
+            [shadowImageView setShadowWithType:EMIShadowPathRoundRectangle shadowColor:[UIColor blackColor] shadowOffset:CGSizeZero shadowOpacity:0.3 shadowRadius:10 image:self.array[index] placeholder:@""];
+        }else{
+//            shadowImageView.backgroundColor = [UIColor whiteColor];
+            [shadowImageView setShadowWithType:EMIShadowPathRoundRectangle shadowColor:[UIColor blackColor] shadowOffset:CGSizeZero shadowOpacity:0.3 shadowRadius:10 image:@"" placeholder:@""];
+            //添加"上传公司证件审核"
+            UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake((pageView.frame.size.width-120)/2, (pageView.frame.size.height-110)/2, 120, 110)];
+            imageView.image = [UIImage imageNamed:@"row_piece_upload_photo"];
+            [shadowImageView addSubview:imageView];
+        }
+        [pageView addSubview:shadowImageView];
     }
     return pageView;
 }
